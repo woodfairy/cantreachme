95bb2bde11144c4f82093ca58d346f962abaa728
Dan Schmit <d.schmit@m3connect.de>
2020-07-07 17:28:19 +0200

    add litten pref bundle (unconfigured)

---
+++ Prefs/AppearanceSettings.m
@@ -0,0 +1,37 @@
+#import "RootListController.h"
+
+@implementation AppearanceSettings
+
+-(UIColor *)tintColor {
+    return [UIColor colorWithRed:0.64 green:0.67 blue:1.00 alpha:1.0];;
+}
+
+-(UIColor *)statusBarTintColor {
+    return [UIColor whiteColor];
+}
+
+-(UIColor *)navigationBarTitleColor {
+    return [UIColor whiteColor];
+}
+
+-(UIColor *)navigationBarTintColor {
+    return [UIColor whiteColor];
+}
+
+-(UIColor *)tableViewCellSeparatorColor {
+    return [UIColor colorWithWhite:0 alpha:0];
+}
+
+-(UIColor *)navigationBarBackgroundColor {
+    return [UIColor colorWithRed:0.64 green:0.67 blue:1.00 alpha:1.0];;
+}
+
+-(BOOL)translucentNavigationBar {
+    return NO;
+}
+
+- (NSUInteger)largeTitleStyle {
+    return 2;
+}
+
+@end
